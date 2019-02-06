e2416be165b8dc5a0901bc31fb0c75bfc45abf31
ayushmaniac <ayushmaniac@gmail.com>
2019-02-06 16:04:53 +0530

    Create StringsReferenceComparison.java

---
+++ StringsReferenceComparison.java
@@ -0,0 +1,17 @@
+
+
+
+class StringsReferenceComparison{
+
+	String s1 = "Ayush";
+	String s2 = "Ayush";
+
+	if(s1==s2){
+
+		System.out.println("String references are equal. ");
+	}
+	else
+	{
+		System.out.println("String references are not equal.")
+	}
+}
