0d0e23225b1b0b2427bd5869de7ab3180b32f4fc
ayushmaniac <ayushmaniac@gmail.com>
2019-02-06 16:10:40 +0530

    Create StringValues.java

---
+++ StringValues.java
@@ -0,0 +1,20 @@
+class StringValues{
+
+public static void main(String[] args){
+String s1 = "Ayush";
+String s2 = "Ayush";
+
+if(s1.equals(s2)){
+
+System.out.println("String values are equal");
+
+}
+else
+{
+System.out.println("String values are notequal");
+
+}
+
+}
+
+}
